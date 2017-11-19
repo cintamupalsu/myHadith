65f0b7d91cfb577ea5b7a4757c66d2d6cb94a8f8
a_maulana <cintamupalsu@hotmail.com>
2017-11-19 10:29:21 +0900

    Signup basic completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'dashboard#home'
   get  '/help' ,   to: 'dashboard#help'
   get  '/about',   to: 'dashboard#about'
   get  '/contact', to: 'dashboard#contact'
   get  '/signup',  to: 'users#new'
-
+  resources :users
 end
