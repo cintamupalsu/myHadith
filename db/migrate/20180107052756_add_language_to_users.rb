4efbca55b654db8ff3932ba4febf229512fefe00
a_maulana <cintamupalsu@hotmail.com>
2018-01-07 14:51:54 +0900

    Try Fix Heroku Reset 03

--- db/migrate/20180107052756_add_language_to_users.rb
+++ db/migrate/20180107052756_add_language_to_users.rb
@@ -1,6 +1,6 @@
 class AddLanguageToUsers < ActiveRecord::Migration[5.1]
   def change
     add_reference :users, :language, index: true
-    add_foreign_key :users, :language
+    add_foreign_key :users, :languages
   end
 end
